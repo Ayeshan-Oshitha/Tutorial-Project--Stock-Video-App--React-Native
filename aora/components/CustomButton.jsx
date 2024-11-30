830db2821eeeb1bd06357296e4a802d962414038
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-11-30 23:17:09 +0530

    6. Onboarding Screen

---
+++ aora/components/CustomButton.jsx
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles}
+       ${isLoading ? "bg-opacity-50" : ""}`}
+      activeOpacity={0.7}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
